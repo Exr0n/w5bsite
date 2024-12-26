a60737a74be3f5d98159c6c06dbf001ecea2da9c
Exr0n <mail@exr0n.com>
2024-12-25 18:19:43 -0800

    add wilderness message, remove old crawling code

--- src/routes/wilderness/[...rest]/+page.svelte
+++ src/routes/wilderness/[...rest]/+page.svelte
@@ -13,14 +13,16 @@
     }
 </style>
   
-<div>
+<div class="space-y-2">
   {#if data && data.files}
     <h1 class="font-sans text-2xl"><Exr0nLink />
         {#each $page.url.pathname.slice(1).split('/') as el, i}
-        <Sep type=">"/>
-        <a href={$page.url.pathname.split('/').slice(0, i+2).join('/')}>{el}</a>
+        <Sep type=">"/><a href={$page.url.pathname.split('/').slice(0, i+2).join('/')}>{el}</a>
         {/each}
     </h1>
+    {#if $page.url.pathname.replace('/', '') === 'wilderness'}
+    <p>Welcome to the wilderness. Feel free to take a look around.</p>
+    {/if}
     <ul>
       {#each data.files as [file, is_dir]}
         <li class="list-disc ml-4 mt-1">
