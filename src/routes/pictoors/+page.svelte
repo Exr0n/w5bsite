e1d403b2c570ba48db589a9774da6bd887c3b097
Exr0n <mail@exr0n.com>
2024-12-29 03:34:55 -0800

    use xl on wide breakpoint

--- src/routes/pictoors/+page.svelte
+++ src/routes/pictoors/+page.svelte
@@ -18,9 +18,10 @@
     let nat_heights = $state({});
 
     let open_pic = $state(null);
-    let open_meta = $derived(open_pic !== null ? pics[open_pic][1] : null)
+    let open_meta = $derived(open_pic !== null ? pics[open_pic][1] : null);
+    let open_is_wide = $derived(nat_widths[open_pic] > nat_heights[open_pic]);
 </script>
-<div class="hidden p-12 space-x-12 space-y-12 sm:flex-row lg:flex-row"></div>   <!-- force tailwind to compile all needed classes -->
+<div class="hidden p-12 space-x-12 space-y-12 sm:flex-row lg:flex-row xl:flex-row"></div>   <!-- force tailwind to compile all needed classes -->
 <svelte:window bind:innerWidth onkeyup={(ev) => { if (ev.key == 'Escape') open_pic = null; }} />
 
 <div class={`flex w-[100vw] space-x-${PHOTO_SPACING} p-${PHOTO_SPACING} sm:px-24 md:px-48 lg:px-72 min-h-[100vh] left-0 top-0 absolute bg-bg`}>
@@ -42,17 +43,23 @@
 </div>
 {#if open_pic !== null}
 <div
-    class={`fixed w-full h-full top-0 left-0 p-4 m-0 animate-in backdrop-blur contain-content fade-in fade-out bg-black bg-opacity-80 flex flex-col ${nat_widths[open_pic] > nat_heights[open_pic] ? "lg" : "sm"}:flex-row items-center justify-center`}
+    class={`fixed w-full h-full top-0 left-0 p-4 m-0 animate-in backdrop-blur contain-content fade-in fade-out bg-black bg-opacity-80 flex flex-col ${open_is_wide ? "xl" : "sm"}:flex-row items-center justify-center`}
     onclick={() => { open_pic = null; }}
     onkeyup={(ev) => { if (ev.key == 'Escape') open_pic = null; }} 
     role="button"
     tabindex="-1"
 >
     <img src={pics[open_pic][0]} class="object-contain max-w-full max-h-full shadow-lg" alt={open_meta.cap}/>
-    <div class="flex-col md:flex-row lg:flex-col font-sans p-2 self-end min-w-max min-h-max">
+    <div class="flex-col md:flex-row xl:flex-col font-sans p-2 self-end min-w-max min-h-max">
         <span class="px-2">{open_meta.cap}</span>
-        <span class="px-2 text-sm block sm:inline-block lg:block"><MapPinAltSolid class="w-3 inline-block -translate-y-[0.1em]" />&nbsp;{open_meta.loc}</span>
-        <span class="pl-2 text-sm block sm:inline-block lg:block">{formatDate(new Date(Date.parse(open_meta.time)))}</span>
+        <span class="px-2 text-sm block sm:inline-block xl:block"><MapPinAltSolid class="w-3 inline-block -translate-y-[0.1em]" />&nbsp;{open_meta.loc}</span>
+        <span class="pl-2 text-sm block sm:inline-block xl:block">{formatDate(new Date(Date.parse(open_meta.time)))}</span>
     </div>
+    <!-- <img src={pics[open_pic][0]} class="object-contain max-w-full max-h-full shadow-lg" alt={open_meta.cap}/>
+    <div class="flex-col ${open_is_wide ? "md" : "xs"}:flex-row ${open_is_wide ? "lg": "md"}:flex-col font-sans p-2 self-end min-w-max min-h-max">
+        <span class="px-2">{open_meta.cap}</span>
+        <span class="px-2 text-sm block ${open_is_wide ? "xs": "md"}:inline-block ${open_is_wide ? "lg" : "md"}:block"><MapPinAltSolid class="w-3 inline-block -translate-y-[0.1em]" />&nbsp;{open_meta.loc}</span>
+        <span class="pl-2 text-sm block ${open_is_wide ? "xs": "md"}:inline-block ${open_is_wide ? "lg" : "md"}:block">{formatDate(new Date(Date.parse(open_meta.time)))}</span>
+    </div> -->
 </div>
 {/if}
