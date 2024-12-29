2d6bc4a172d6e0ed666a53543c25f246b5209a4a
Exr0n <mail@exr0n.com>
2024-12-29 03:17:03 -0800

    FIX: single-column wasn't showing all photos

--- src/routes/pictoors/+page.svelte
+++ src/routes/pictoors/+page.svelte
@@ -11,6 +11,9 @@
 
     const PHOTO_SPACING = 12
 
+    let innerWidth = $state(0);
+    const breakpoint_lg = 1024;  // px, from tailwind
+
     let nat_widths = $state({});
     let nat_heights = $state({});
 
@@ -18,11 +21,12 @@
     let open_meta = $derived(open_pic !== null ? pics[open_pic][1] : null)
 </script>
 <div class="hidden p-12 space-x-12 space-y-12 sm:flex-row lg:flex-row"></div>   <!-- force tailwind to compile all needed classes -->
+<svelte:window bind:innerWidth onkeyup={(ev) => { if (ev.key == 'Escape') open_pic = null; }} />
 
 <div class={`flex w-[100vw] space-x-${PHOTO_SPACING} p-${PHOTO_SPACING} sm:px-24 md:px-48 lg:px-72 min-h-[100vh] left-0 top-0 absolute bg-bg`}>
     <div class={`flex flex-col space-y-${PHOTO_SPACING} w-full lg:w-1/2`}>
         <Breadcrumbs label="pictoors" />
-        {#each pics.map((x, i) => [x, i]).filter(([_, i]) => i % 2) as [[path, meta], i]}
+        {#each pics.map((x, i) => [x, i]).filter(([_, i]) => (innerWidth < breakpoint_lg ? true : i % 2)) as [[path, meta], i]}
         <button onclick={ () => { open_pic = i; } } aria-label="Expand">
             <img src={path} class="object-contain" bind:naturalWidth={nat_widths[i]} bind:naturalHeight={nat_heights[i]} alt={meta.cap} />
         </button>
@@ -52,4 +56,3 @@
     </div>
 </div>
 {/if}
-<svelte:window onkeyup={(ev) => { if (ev.key == 'Escape') open_pic = null; }} />
