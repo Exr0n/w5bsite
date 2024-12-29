f3f7262706d55727a2590910208f461982539029
Exr0n <mail@exr0n.com>
2024-12-29 03:11:58 -0800

    FIX: use pic id bc paths aren't consistent in build env

--- src/routes/pictoors/+page.svelte
+++ src/routes/pictoors/+page.svelte
@@ -8,30 +8,30 @@
     const { data } = $props();
 
     const pics = data.pics;
+
     const PHOTO_SPACING = 12
 
     let nat_widths = $state({});
     let nat_heights = $state({});
-    let x = $derived(console.log(nat_widths))
 
     let open_pic = $state(null);
-    let open_meta = $derived(open_pic !== null ? metadata[open_pic.split('/').at(-1).split('?').at(0)] : null)
+    let open_meta = $derived(open_pic !== null ? pics[open_pic][1] : null)
 </script>
-<div class="hidden p-12 space-x-12 space-y-12 sm:flex-row lg:flex-row" />   <!-- force tailwind to compile all needed classes -->
+<div class="hidden p-12 space-x-12 space-y-12 sm:flex-row lg:flex-row"></div>   <!-- force tailwind to compile all needed classes -->
 
 <div class={`flex w-[100vw] space-x-${PHOTO_SPACING} p-${PHOTO_SPACING} sm:px-24 md:px-48 lg:px-72 min-h-[100vh] left-0 top-0 absolute bg-bg`}>
     <div class={`flex flex-col space-y-${PHOTO_SPACING} w-full lg:w-1/2`}>
         <Breadcrumbs label="pictoors" />
         {#each pics.map((x, i) => [x, i]).filter(([_, i]) => i % 2) as [[path, meta], i]}
-        <button onclick={ () => { open_pic = path } } aria-label="Expand">
-            <img src={path} class="object-contain" bind:naturalWidth={nat_widths[path]} bind:naturalHeight={nat_heights[path]} alt={meta.cap} />
+        <button onclick={ () => { open_pic = i; } } aria-label="Expand">
+            <img src={path} class="object-contain" bind:naturalWidth={nat_widths[i]} bind:naturalHeight={nat_heights[i]} alt={meta.cap} />
         </button>
         {/each}
     </div>
     <div class={`flex flex-col space-y-${PHOTO_SPACING} hidden lg:block w-1/2`}>
         {#each pics.map((x, i) => [x, i]).filter(([_, i]) => !(i % 2)) as [[path, meta], i]}
-        <button onclick={ () => { open_pic = path } } aria-label="Expand">
-            <img src={path} class="object-contain" bind:naturalWidth={nat_widths[path]} bind:naturalHeight={nat_heights[path]} alt={meta.cap} />
+        <button onclick={ () => { open_pic = i } } aria-label="Expand">
+            <img src={path} class="object-contain" bind:naturalWidth={nat_widths[i]} bind:naturalHeight={nat_heights[i]} alt={meta.cap} />
         </button>
         {/each}
     </div>
@@ -44,7 +44,7 @@
     role="button"
     tabindex="-1"
 >
-    <img src={open_pic} class="object-contain max-w-full max-h-full shadow-lg" onload={console.log(nat_heights)} alt={open_meta.cap}/>
+    <img src={pics[open_pic][0]} class="object-contain max-w-full max-h-full shadow-lg" alt={open_meta.cap}/>
     <div class="flex-col md:flex-row lg:flex-col font-sans p-4 self-end min-w-max min-h-max">
         <p>{open_meta.cap}</p>
         <p class="text-sm"><MapPinAltSolid class="w-3 inline-block -translate-y-[0.1em]" />&nbsp;{open_meta.loc}</p>
